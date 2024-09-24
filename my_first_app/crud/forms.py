fe12a915ec35b294d07d99756e5f4a23b15f5158
SmilingWitch <danielitayero@gmail.com>
2024-09-24 18:32:38 -0300

    feat: add widgets to forms.py

--- my_first_app/crud/forms.py
+++ my_first_app/crud/forms.py
@@ -5,3 +5,7 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows':3})
+        }
